69e6447e94414b2ed31cdb57d746a3d65fef6c6a
iroguin <143116756+Iroguin@users.noreply.github.com>
2025-09-04 13:00:48 +0300

    added invoke to run server and instructions

---
+++ documentation/Instructions.md
@@ -0,0 +1,19 @@
+# User Instructions
+
+## Installation
+Clone the Repository
+In the root of the project run:
+```
+Poetry install
+```
+then:
+```
+eval $(poetry env activate)
+```
+
+
+## run server
+use command:
+```
+invoke server
+```
--- tasks.py
+++ tasks.py
@@ -1,1 +1,9 @@
 """All invoke commands to be put here"""
+from invoke import task
+
+@task
+def server(c):
+    """Run local server via vite"""
+    with c.cd('frontend'):
+        c.run(f'npm install')
+        c.run('npm run dev -- --open')
