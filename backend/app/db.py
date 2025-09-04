251d007c8d4474ba79d898887dc2d4edd2b7fc8a
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2025-09-04 16:11:24 +0300

    Added password to db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://wheel_db_user:<db_password>@cluster0.wxszkjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://wheel_db_user:vA3xhOjm47ifxGpK@cluster0.wxszkjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
