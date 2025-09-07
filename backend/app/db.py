c4a9bb01c7aaac6591ef01ed3e9ec3b125ee8d11
Aleksi Saxlund <aleksi.saxlund@gmail.com>
2025-09-07 15:59:16 +0300

    Added dotenv to poetry and load_dotenv() to db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 from os import getenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+load_dotenv()
 uri = getenv("MONGO_URI")
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
