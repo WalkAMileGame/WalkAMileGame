21db9126d29b48e03d91cf7e436467e1e947ce24
Simona <simonacallegari@outlook.com>
2025-09-08 17:56:04 +0300

    adding contents to db

--- backend/app/db.py
+++ backend/app/db.py
@@ -8,12 +8,15 @@ uri = getenv("MONGO_URI")
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
-db = client["thewheel"]
+db = client.get_database()
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
     
+
+db.points.insert_one({"id": "0", "values": 32})
 collections = db.list_collection_names()
 print("Collections in DB:", collections)
+#output: Collections in DB: ['users', 'movies', 'embedded_movies', 'points', 'comments', 'theaters', 'sessions'] <-- nää postetaan jossainvaiheessa.
