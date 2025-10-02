ada1b8c2a88b2dceb7ac4953f45a041f210a1acd
Aleksi Saxlund <aleksi.saxlund@gmail.com>
2025-10-02 14:44:01 +0300

    More login page tests

--- frontend/src/__tests__/Login.test.jsx
+++ frontend/src/__tests__/Login.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../components/Login';
 
 test('renders login form', () => {
@@ -8,3 +9,20 @@ test('renders login form', () => {
 
     expect(linkElement).toBeInTheDocument()
 })
+
+test ('form calls login submission handler with correct credentials', async () => {
+    const handleSubmit = vi.fn()
+    const user = userEvent.setup()
+    render (<Login onSubmit={handleSubmit}/>)
+
+    await user.type(screen.getByLabelText(/email/i), 'test@example.com')
+    await user.type(screen.getByLabelText(/password/i), 'salainensalasana123')
+    await user.click(screen.getByRole('button', {name: /login/i}))
+
+    expect(handleSubmit).toHaveBeenCalledTimes(1)
+    expect(handleSubmit).toHaveBeenCalledWith({
+        email: 'test@example.com',
+        password: 'salainensalasana123',
+    })
+})
+
