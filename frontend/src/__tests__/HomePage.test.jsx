e5308b48d88b9e2de76219b7f9f168c19ff81196
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2025-10-02 13:05:13 +0300

    Testing dependencies added

---
+++ frontend/src/__tests__/HomePage.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import HomePage from '../components/HomePage'
+
+test('renders content', () => {
+
+
+    const element = screen.getByTet('The Wheel Homepage')
+})
