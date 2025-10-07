0dad7b42960a40a67f963bcf4fe8605da77aa0cd
Oskari <oskari.lamsa@helsinki.fi>
2025-10-08 00:45:43 +0300

    In this commit, several yaml files regarding the frontend deployment are changed. Also, vite.config.js file is changed to include the server settings, used by the deployment. Notably, it allows hosts from all addresses, which is set for the purposes of local development. This must be changed before the final version of the production version. With these changes, the frontend deployment should now be accessible.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // allowedHosts allows everything for the purposes of local developement. This is NOT an acceptable solution for
+    // the final production version.
+    allowedHosts: true
+  },
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.js',     
+    setupFiles: './src/setupTests.js'
   }
 })
-
-
-
