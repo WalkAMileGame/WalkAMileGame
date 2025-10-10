1910df28a56aeeac7e9aa858e0112f6efdb62905
Oskari <oskari.lamsa@helsinki.fi>
2025-10-10 15:42:52 +0300

    I want to merge all to main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,23 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // allowedHosts: true allows everything for the purposes of local developement. This is NOT an acceptable solution for
+    // the final production version.
+    allowedHosts: true
+  },
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.js',     
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      exclude: [
-        'src/components/ui/snackbar.jsx',
-        'src/setupTests.js',
-        'src/main.jsx',
-        'vite.config.js',
-        'eslint.config.js'
-      ],
-    },
-  }
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: './src/setupTests.js',     
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'lcov'],
+			exclude: [
+				'src/components/ui/snackbar.jsx',
+				'src/setupTests.js',
+				'src/main.jsx',
+				'vite.config.js',
+				'eslint.config.js'
+			],
+		},
+	}
 })
-
-
-
