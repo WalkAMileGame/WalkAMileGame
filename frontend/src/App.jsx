44701420784331be7e71c4d84ae9c9b0bf322693
Simona <simonacallegari@outlook.com>
2025-10-16 19:54:25 +0300

    landing page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import {useEffect, useState } from 'react'
+// import {useEffect, useState } from 'react'
 // import myLogo from './assets/GB.png'
 import HomePage from './components/HomePage';
 import GameBoard from "./components/GameBoard";
 import Login from './components/Login';
-// import './styles/App.css';
-// import GameBoard from "./GameBoard.jsx"
+import LandingPage from './components/LadingPage';
+
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
 function App() {
@@ -25,7 +25,8 @@ function App() {
         <Routes>
           <Route path="/gameboard" element={<GameBoard />} />
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} /> {/* if admin not logged in */}
+          <Route path="/landing" element={<LandingPage />} /> {/* if admin logged in show link 'admin panel' or something */}
         </Routes>
       </Router>
     </>
---
+++ frontend/src/components/LadingPage.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import '../styles/LandingPage.css';
+
+
+
+const LandingPage = () => {
+    return (
+      <div className="landing-text">
+        <header>
+          <h1>CONTROL PANEL</h1>
+        </header>
+        <hr></hr>
+        <main>
+          <h2>You can edit gameboard templates and host a game here</h2>
+        </main>
+
+        <div className="buttons">
+            <Link to="/gameboard">
+                <button>🎲 EDIT GAMEBOARDS</button>
+            </Link>
+            <Link to="/landing/#">
+                <button>🕹️ HOST A GAME</button>
+            </Link>
+            <Link to="/landing/#">
+                <button>👤 EDIT USERS</button> {/* This needs the admin authentication */}
+            </Link>
+        </div>
+
+
+      </div>
+  );
+};
+
+export default LandingPage;
