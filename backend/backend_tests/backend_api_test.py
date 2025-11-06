4fd6278727b07bbc4299672f91c1187904d45498
Aleksi Saxlund <aleksi.saxlund@gmail.com>
2025-11-06 15:39:10 +0200

    Added missing tests for login and authentication system

--- backend/backend_tests/backend_api_test.py
+++ backend/backend_tests/backend_api_test.py
@@ -4,6 +4,7 @@ from unittest.mock import patch, MagicMock
 import pytest
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
+from backend.app.security import get_current_active_user
 
 
 os.environ['TESTING'] = 'true'
@@ -152,3 +153,186 @@ def test_health_check_various_db_errors(mock_db_instance):
         response_data = response.json()
         assert response_data["status"] == "unhealthy"
         assert error_msg in response_data["message"]
+
+
+@patch('backend.app.api.db')
+def test_login_user_doesnt_exist(mock_db_instance):
+    """Test login attempt with non-existing user"""
+
+    mock_db_instance.users.find_one.return_value = None
+
+    response = client.post("/login", json={"email": "test@example.com", "password": "password"})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Incorrect email or password"}
+
+    mock_db_instance.users.find_one.assert_called_once_with({"email": "test@example.com"})
+
+
+@patch('backend.app.api.verify_password')
+@patch('backend.app.api.db')
+def test_login_wrong_password_or_email(mock_db_instance, mock_verify_password):
+    """Test login attempt with incorrect credentials"""
+
+    mock_db_instance.users.find_one.return_value = {
+        "_id": "mock_id",
+        "email": "test@example.com",
+        "password": "mock_hashed_password",
+        "role": "admin",
+        "pending": False
+        }
+    
+    mock_verify_password.return_value = False
+
+    response = client.post(
+        "/login",
+        json={"email": "test@example.com", "password": "password123"}
+        )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Incorrect email or password"}
+
+    mock_db_instance.users.find_one.assert_called_once_with({"email": "test@example.com"})
+
+@patch('backend.app.api.verify_password')
+@patch('backend.app.api.db')
+def test_login_account_pending_to_be_accepted(mock_db_instance, mock_verify_password):
+    """Test login attempt with account still pending to be accepted"""
+
+    mock_db_instance.users.find_one.return_value = {
+        "_id": "mock_id",
+        "email": "test@example.com",
+        "password": "mock_hashed_password",
+        "role": "admin",
+        "pending": True
+        }
+    
+    mock_verify_password.return_value = True
+
+    response = client.post(
+        "/login",
+        json={"email": "test@example.com", "password": "password123"}
+        )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Account still pending to be accepted"}
+
+    mock_db_instance.users.find_one.assert_called_once_with({"email": "test@example.com"})
+
+@patch('backend.app.api.create_access_token')
+@patch('backend.app.api.verify_password')
+@patch('backend.app.api.db')
+def test_login_successful_login_attempt(mock_db_instance, mock_verify_password, mock_create_access_token):
+    """Test successful login attempt"""
+
+    mock_db_instance.users.find_one.return_value = {
+        "_id": "mock_id",
+        "email": "test@example.com",
+        "password": "mock_hashed_password",
+        "role": "admin",
+        "pending": False
+        }
+    
+    mock_verify_password.return_value = True
+
+    mock_create_access_token.return_value = "mock_access_token"
+
+    response = client.post(
+        "/login",
+        json={"email": "test@example.com", "password": "password123"}
+        )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "access_token": "mock_access_token",
+        "user": {"email": "test@example.com", "role": "admin"}
+        }
+
+    mock_db_instance.users.find_one.assert_called_once_with({"email": "test@example.com"})
+
+
+@patch('backend.app.api.db')
+def test_register_email_already_in_use(mock_db_instance):
+    """Test register attempt with already existing email"""
+
+    mock_db_instance.users.find_one.return_value = {
+        "_id": "mock_id",
+        "email": "test@example.com",
+        "password": "mock_hashed_password",
+        "role": "admin",
+        "pending": False
+        }
+    
+    response = client.post(
+        "/register",
+        json={"email": "test@example.com", "password": "password123"}
+        )
+    
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Email already in use"}
+
+    mock_db_instance.users.find_one.assert_called_once_with({"email": "test@example.com"})
+
+
+@patch('backend.app.api.db')
+def test_register_invalid_email(mock_db_instance):
+    """Test register attempt with invalid email"""
+
+    mock_db_instance.users.find_one.return_value = None
+    
+    response = client.post(
+        "/register",
+        json={"email": "test.example.com", "password": "password123"}
+        )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == "value is not a valid email address: An email address must have an @-sign."
+
+
+@patch('backend.app.api.get_password_hash')
+@patch('backend.app.api.db')
+def test_register_successful_register_attempt(mock_db_instance, mock_get_password_hash):
+    """Test registering successfully"""
+
+    mock_db_instance.users.find_one.return_value = None
+
+    mock_db_instance.users.update_one.return_value = MagicMock(upserted_id="123")
+
+    mock_get_password_hash.return_value = "mock_password_hash"
+    
+    response = client.post(
+        "/register",
+        json={"email": "test@example.com", "password": "password123"}
+        )
+    
+    assert response.status_code == 200
+
+    mock_db_instance.users.update_one.assert_called_once_with(
+        {"email": "test@example.com"},
+        {
+        "$set": {
+            "email": "test@example.com",
+            "password": "mock_password_hash",
+            "role": "admin",
+            "pending": True
+            }
+        },
+        upsert = True
+    )
+
+
+def test_read_current_user():
+    """Test checking the logged in user"""
+    mock_user = {"email": "test@example.com", "role": "admin"}
+
+    def override_get_user():
+        return mock_user
+
+    app.dependency_overrides[get_current_active_user] = override_get_user
+
+    response = client.get("/users/me")
+
+    assert response.status_code == 200
+    assert response.json() == mock_user
+
+    app.dependency_overrides = {}
+
