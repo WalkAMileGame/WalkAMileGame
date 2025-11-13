f41d57c8deace2b6e5ce8ee8f488384ef3a03b97
Simona <simonacallegari@outlook.com>
2025-11-13 14:35:32 +0200

    fixes

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -79,9 +79,6 @@ const HomePage = () => {
           <svg viewBox="0 0 500 500">
           <path id="curve" fill="transparent" stroke="none" d="M73.2,105.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
             <text width="300" fontSize="18" fill="white" fontFamily="Lilita One, sans-serif">
-              <textPath href="#curve" startOffset="50%" textAnchor="middle">
-                START
-              </textPath>
             </text>
         </svg>
           <div className="start">
