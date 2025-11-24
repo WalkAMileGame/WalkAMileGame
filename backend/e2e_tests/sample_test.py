ac2b3f29567cc8c24dd30e45a45c44f6cc62ecbf
Oskari <oskari.lamsa@helsinki.fi>
2025-11-24 10:05:37 +0200

    E2E tests

--- backend/e2e_tests/sample_test.py
+++ backend/e2e_tests/sample_test.py
@@ -7,6 +7,22 @@ def browser():
         page = browser.new_page()
         yield page
         browser.close()
-def test_site_is_up(browser, servers):
+def test_frontend_is_up(browser, servers):
     browser.goto("localhost:5173")
     assert browser.title() == ""
+
+def test_backend_is_up(browser, servers):
+    browser.goto("localhost:8000")
+    assert browser.title() == ""
+    
+def test_go_to_login(browser, servers):
+    browser.goto("localhost:5173")
+    browser.get_by_role("link", name="Login").click()
+    assert browser.url == "http://localhost:5173/login"
+
+def test_login(browser, servers):
+    browser.goto("localhost:5173")
+    browser.get_by_role("link", name="Login").click()
+    locator = browser.locator("#email")
+    locator.fill("opettaja@test.fi")
+    assert locator.input_value() == "opettaja@test.fi"
